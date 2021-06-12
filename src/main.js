e47dd687c3bcf9ee5d7b97eeeed39d9672929ea7
Jason <kangja1996@gmail.com>
2021-06-12 14:45:18 -0400

    progress

--- src/main.js
+++ src/main.js
@@ -1,21 +1,26 @@
 const { Blockchain, Transaction } = require("./blockchain");
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
+
+const myKey = ec.keyFromPrivate(
+  "ee3c4557319af9f750e509977d99c5f474ddd75dabb1d1de25b0a68418f3a228"
+);
+const myWalletAddress = myKey.getPublic("hex");
 
 let kangCoin = new Blockchain();
-kangCoin.createTransaction(new Transaction("address1", "address2", 100));
-kangCoin.createTransaction(new Transaction("address2", "address1", 50));
+
+const tx1 = new Transaction(myWalletAddress, "public key goes here", 10);
+tx1.signTransaction(myKey);
+kangCoin.addTransaction(tx1);
 
 console.log("\nStarting the miner...");
-kangCoin.minePendingTransactions("josh-address");
+kangCoin.minePendingTransactions(myWalletAddress);
 
 console.log(
   "\nBalance of josh is",
-  kangCoin.getBalanceOfAddress("josh-address")
+  kangCoin.getBalanceOfAddress(myWalletAddress)
 );
 
-console.log("\nStarting the miner again...");
-kangCoin.minePendingTransactions("josh-address");
+kangCoin.chain[1].transactions[0].amount = 1;
 
-console.log(
-  "\nBalance of josh is",
-  kangCoin.getBalanceOfAddress("josh-address")
-);
+console.log("is chain valid?", kangCoin.isChainValid());
